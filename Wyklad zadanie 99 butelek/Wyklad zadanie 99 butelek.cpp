5154788a8fce3729b2c759ce6108996d95058e54
Anna Prymas <s24284@pjwstk.edu.pl>
2021-11-03 18:56:14 +0100

    Dodaj pliki projektów.

---
+++ Wyklad zadanie 99 butelek/Wyklad zadanie 99 butelek.cpp
@@ -0,0 +1,34 @@
+﻿// Wyklad zadanie 99 butelek.cpp : 
+//
+
+#include <iostream>
+
+int main()
+{
+    int liczbabutelek;    
+
+    do
+    {
+    std::cout << "Podaj, od ilu butelek zaczynamy spiewac. Zakres to 1-99:\n";
+    std::cin >> liczbabutelek;
+    if (liczbabutelek < 1 || liczbabutelek > 99)
+    {
+        std::cout << "Nie podales liczby z zakresu 1-99. Sprobuj jeszcze raz.\n";
+    }
+    }     while (liczbabutelek < 1 || liczbabutelek > 99);
+       
+
+    for (int i = liczbabutelek; i > 0; i--)
+    {
+        std::cout << i << " bottles of beer on the wall, " << i << " bottles of beer. Take one down, pass it around, " << i - 1 << " bottles of beer on the wall...\n";
+
+        
+    }     
+     
+            std::cout << "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...\n";
+           
+    
+    return 0;
+
+}
+
